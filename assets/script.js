3cc2723d6e6fc38287c52a0f0330223c5ded9e62
jwright323 <54717725+jwright323@users.noreply.github.com>
2020-10-11 20:59:03 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 document.querySelector("#generate").addEventListener("click", writePW);
 
 //Types of characters to be used to create passwords.
-const pwAbcUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const pwAbcLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const pwNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const alphaUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const alphaLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const pwSpecChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
 
 //Variables
